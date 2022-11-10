45dee4058746fe2b1456d197a9073cff43b74bb9
melekabbassi <abbassimelek@gmail.com>
2022-11-10 20:06:17 +0100

    same

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import cv2
+import face_recognition
 
 image = cv2.imread('Haaland.jpg')
+rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+image_encoding = face_recognition.face_encodings(rgb)[0]
 
 cv2.imshow('image', image)
 
