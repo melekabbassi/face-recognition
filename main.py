598a43a04b0a182e20cdffad3322dd774d78c784
melekabbassi <abbassimelek@gmail.com>
2022-11-10 20:22:39 +0100

    same

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import cv2
 import face_recognition
 
-image = cv2.imread('Haaland.jpg')
-rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-image_encoding = face_recognition.face_encodings(rgb)[0]
+image1 = cv2.imread('images/Haaland.jpg')
+rgb1 = cv2.cvtColor(image1, cv2.COLOR_BGR2RGB)
+image1_encoding = face_recognition.face_encodings(rgb1)[0]
 
-cv2.imshow('image', image)
+image2 = cv2.imread('images/Elon.jpg')
+rgb2 = cv2.cvtColor(image2, cv2.COLOR_BGR2RGB)
+image1_encoding = face_recognition.face_encodings(rgb2)[0]
+
+cv2.imshow('image1', image1)
+cv2.imshow('image2', image2)
 
 cv2.waitKey(0)
