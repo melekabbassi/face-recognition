ea3614a81b5f73bc3cdf497964054252aeea5bfd
melekabbassi <abbassimelek@gmail.com>
2022-11-10 20:34:35 +0100

    works fine

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ image1 = cv2.imread('images/Haaland.jpg')
 rgb1 = cv2.cvtColor(image1, cv2.COLOR_BGR2RGB)
 image1_encoding = face_recognition.face_encodings(rgb1)[0]
 
-image2 = cv2.imread('images/Elon.jpg')
+image2 = cv2.imread('images/Haaland3.png')
 rgb2 = cv2.cvtColor(image2, cv2.COLOR_BGR2RGB)
-image1_encoding = face_recognition.face_encodings(rgb2)[0]
+image2_encoding = face_recognition.face_encodings(rgb2)[0]
 
-cv2.imshow('image1', image1)
-cv2.imshow('image2', image2)
+result = face_recognition.compare_faces([image1_encoding], image2_encoding)
+
+print(result)
 
 cv2.waitKey(0)
