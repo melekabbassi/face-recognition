f35754a9429b64d2efd6f0e73bd93271e06a8a34
melekabbassi <abbassimelek@gmail.com>
2022-11-11 16:54:30 +0100

    same

--- web.py
+++ web.py
@@ -5,3 +5,8 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/face_recognition")
+def face_recognition():
+    return ("<p>Face Recognition</p>"
+            "<p>Face Recognition</p>")
