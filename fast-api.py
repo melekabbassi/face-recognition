d293d8e5bcac8019310d0ec984137a284e9f2b46
melekabbassi <abbassimelek@gmail.com>
2022-11-18 00:40:53 +0100

    added cors middleware but still the same

--- fast-api.py
+++ fast-api.py
@@ -1,10 +1,26 @@
 from fastapi import FastAPI
 import pickle
+from fastapi.middleware.cors import CORSMiddleware
 
 matches = pickle.load(open("matches.pkl", "rb"))
 
 app = FastAPI()
- 
+
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "http://localhost:3000",
+    "http://localhost:3001",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/matches")
 def get_matches():
     return {"matches": matches}
